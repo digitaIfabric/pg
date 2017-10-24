425b8c3abd3b541fae2e7db7bcf03484b68b4178
digitalfabric92 <dwawryko@gmail.com>
2017-10-24 16:49:14 +0000

    Added query

---
+++ test_script.js
@@ -0,0 +1,31 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+let input = process.argv[2].split('').join('');
+
+const client = new pg.Client({
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+});
+
+client.connect((err) => {
+    if (err) {
+        return console.error("Connection Error", err);
+    }
+    client.query("SELECT * FROM famous_people WHERE last_name LIKE $1 OR first_name LIKE $1", [`%${input}%`], (err, result) => {
+    if (err) {
+        return console.error("error running query", err);
+    }
+    console.log("Searching...");
+    console.log(`Found ${result.rows.length} person(s) by the name '${input}':`);
+    for (let row of result.rows) {
+    console.log(`- ${row.id} : ${row.first_name} ${row.last_name} born ${row.birthdate}`);
+    }
+client.end();
+});
+});
+
+
